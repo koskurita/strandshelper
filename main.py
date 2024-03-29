29d20356541919fb3719435592ae5e4a5b3dd1ec
koskurita <kosuke.kurita@gmail.com>
2024-03-29 16:12:05 -0700

    add matrix class

--- main.py
+++ main.py
@@ -24,6 +24,12 @@
 # Trie implementation 2: nodes
 
 import json
+from bs4 import BeautifulSoup
+import requests
+from time import sleep
+from selenium import webdriver
+
+_STRANDS_URL = "https://www.nytimes.com/games/strands"
 
 class TrieNode:
     def __init__(self, letter):
@@ -108,54 +114,65 @@ class Trie:
                 self.add_word(word)
         self.to_JSON(fijs)
 
+class StrandsMatrix:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self._rowSize = 6
+        self._columnSize = 8
+        self.charMatrix = [["a" for x in range(self._rowSize)] for y in range(self._columnSize)] 
+        self.visitedMatrix = [[False for x in range(self._rowSize)] for y in range(self._columnSize)] 
 
+
+    def scrape(self):
+        letter_list = []
+        _url = _STRANDS_URL 
+        driver = webdriver.Firefox()
+        driver.get(_url)
+        sleep(3)
+        source = driver.page_source
+        soup = BeautifulSoup(source, 'lxml')
+        letters = soup.find_all('button', class_= "pRjvKq_item")
+        for letter in letters:
+            letter_list.append(letter.get_text())
+        self.createMatrix(letter_list)
+        return 
+
+    def createMatrix(self, letter_list):
+        cnt = 0
+        for i in range(self._columnSize):
+            for j in range(self._rowSize):
+                self.charMatrix[i][j] = letter_list[cnt]
+                cnt += 1
+        return 
+
+    def solveMatrix(self, x, y, trie, word, possible_words):
+        if self.charMatrix[x][y] not in trie.children:
+            return
+        word.append(self.charMatrix[x][y])
+        next_trie = trie.children[self.charMatrix[x][y]]
+        self.visited[x][y] = True
+        if next_trie.is_end_of_word:
+            possible_words.append(word)
+        dx = [-1, -1, -1, 0, 0, 1, 1, 1]
+        dy = [-1, 0, 1, -1, 1, -1, 0, 1]
+        for i in range(len(dx)):
+            next_x = x + dx[i]
+            next_y = y + dy[i]
+            if self.check_boundaries(next_x, next_y) and not self.visitedMatrix[next_x][next_y]:
+                self.solveMatrix(next_x, next_y, next_trie, word, possible_words)
+        self.visited[x][y] = False
+        del word[-1]
+
+    def check_boundaries(self, x, y):
+        return x >= 0 and x < self._rowSize and y >= 0 and y < self._columnSize
+            
 
 
 
 d_trie = Trie()
-d_trie.parse_dict_to_Tree("new_dictionary.txt", "dictionary.json")
 
-
-
-
-# new_tree = Trie()
-# new_tree.load_JSON()
-# print("\n")
-# new_tree.print_trie()
-# print("\n")
-
-
-# s = d_trie.to_JSON()
-
-
-
-
-# d_trie.print_trie()
-
-
-# with open("dictionary.json", "w") as f:
-#     json.dump(d_trie, f)
-
-
-
-
-
-
-# _rowSize = 6
-# _columnSize = 8
-
-# charMatrix = [["a" for x in range(_rowSize)] for y in range(_columnSize)] 
-# visitedMatrix = [[False for x in range(_rowSize)] for y in range(_columnSize)] 
-
-# cnt = 0
-# for i in range(_columnSize):
-#     for j in range(_rowSize):
-#         charMatrix[i][j] = chr(cnt+ord("a"))
-#         cnt+=1
-#         if cnt >= 26:
-#             cnt = 0
-#         print(i, j, cnt)
-# print(charMatrix)
-
-# def backtrack(x, y, trie, word, possible_words):
-#     print("hi")
+m = StrandsMatrix()
+m.scrape()
+print(m.charMatrix)
+print(m.visitedMatrix)
